b001d27a7958e739d68aabf8f75fffb66a3dcaa6
Gustavo <gust.bersan@gmail.com>
2023-01-30 22:39:31 -0300

    Teste no método que imprime o tabuleiro

--- ChessmateConsole/Program.cs
+++ ChessmateConsole/Program.cs
@@ -8,6 +8,9 @@ namespace ChessmateConsole
         static void Main(string[] args)
         {
             Board board = new Board(8, 8);
+
+            Screen.PrintBoard(board);
+
             Console.ReadKey();
         }
     }
