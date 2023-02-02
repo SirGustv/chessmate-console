56af06f02e79edfdd3442da66df36df392c347cc
Gustavo <gust.bersan@gmail.com>
2023-02-02 18:41:12 -0300

    Adicionando método para ler a posição
    Método irá ler uma determinada posição (origem e destino) para as peças poderem se mover de acordo com o que foi especificado pelo usuário.

--- ChessmateConsole/Screen.cs
+++ ChessmateConsole/Screen.cs
@@ -1,5 +1,6 @@
 using System;
 using GameBoard;
+using Chessmate;
 
 namespace ChessmateConsole
 {
@@ -25,6 +26,14 @@ namespace ChessmateConsole
                 Console.WriteLine();
             }
             Console.WriteLine("  a b c d e f g h");
+        }
+
+        public static ChessPosition ReadPositionChess()
+        {
+            string s = Console.ReadLine();
+            char column = s[0];
+            int line = int.Parse(s[1] + "");
+            return new ChessPosition(column, line);
         }
 
         public static void PrintPiece(Piece piece)
